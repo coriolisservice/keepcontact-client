8e21eae7d27db4157a5f8100185377d90c13d0ac
Coriolis Service <maximecaze@coriolis.fr>
2017-07-10 15:34:08 +0200

    azerty

--- tests/test.php
+++ tests/test.php
@@ -2,17 +2,17 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
-$uri = 'https://www.coriolis-web.fr/keep-contact/api/';
-$login = 'direxi';
-$password = '';
+$uri = 'https://keepcontact_api_url/';
+$login = 'login';
+$password = 'password';
 
 try {
     $client = new \KeepContact\KeepContactClient($uri, $login, $password); 
 
     $client->sendSms(
-        '0683171556', 
+        '0601020304', 
         "test", 
-        "DIREXI"
+        "INFOSMS"
     );
 } catch (Exception $e) {
     echo 'Une erreur est survenue : ' . $e->getMessage() . ' (code : ' . $e->getCode() . ')';
